a41bd4b2c87c81e7968d111eb8bb7997b8397abf
yuhan.tang <yuhan.tang@magicwindow.cn>
2018-07-19 11:07:11 +0800

    init

--- entertainment-asset/src/main/java/com/entertainment/asset/api/TestController.java
+++ entertainment-asset/src/main/java/com/entertainment/asset/api/TestController.java
@@ -1,11 +1,10 @@
 package com.entertainment.asset.api;
 
 import com.entertainment.asset.service.TestService;
-import com.entertainment.common.entity.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,9 +16,9 @@ public class TestController {
     @Autowired
     private TestService testService;
 
-    @RequestMapping(path = "/find/{key}" , method = {RequestMethod.GET})
-    public Object init(HttpServletRequest request,
-                     @RequestParam("key") String key){
-        return testService.findByKey(key);
+    @RequestMapping(path = "/find/{ke}" , method = {RequestMethod.GET})
+    public Object aaaa(HttpServletRequest request,
+                     @PathVariable("ke") String ke){
+        return testService.findByKey(ke);
     }
 }
--- entertainment-common/src/main/java/com/entertainment/common/base/BaseEntityRepository.java
+++ entertainment-common/src/main/java/com/entertainment/common/base/BaseEntityRepository.java
@@ -1,5 +1,6 @@
 package com.entertainment.common.base;
 
+import com.entertainment.common.entity.base.BaseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
