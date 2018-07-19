83be55d4d1b9437995567cda4209e786cd2fd3d3
yuhan.tang <yuhan.tang@magicwindow.cn>
2018-07-19 16:54:00 +0800

    add redis序列化

--- entertainment-asset/src/main/java/com/entertainment/asset/api/TestController.java
+++ entertainment-asset/src/main/java/com/entertainment/asset/api/TestController.java
@@ -2,6 +2,7 @@ package com.entertainment.asset.api;
 
 import com.entertainment.asset.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,9 +17,21 @@ public class TestController {
     @Autowired
     private TestService testService;
 
+    @Autowired
+    private RedisTemplate redisTemplateJackson;
+
     @RequestMapping(path = "/find/{ke}" , method = {RequestMethod.GET})
     public Object aaaa(HttpServletRequest request,
                      @PathVariable("ke") String ke){
         return testService.findByKey(ke);
+    }
+
+    @RequestMapping(path = "/get/{key}/{value}", method = {RequestMethod.GET})
+    public Object get(HttpServletRequest request,
+                      @PathVariable("key") String key,
+                      @PathVariable("value") String value){
+        redisTemplateJackson.opsForValue().set(key,value);
+        String keyValue = redisTemplateJackson.opsForValue().get(key).toString();
+        return keyValue;
     }
 }
---
+++ entertainment-asset/src/main/java/com/entertainment/asset/config/RedisConfig.java
@@ -0,0 +1,44 @@
+package com.entertainment.asset.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Data
+@Configuration
+public class RedisConfig {
+
+    @Autowired
+    private LettuceConnectionFactory lettuceConnectionFactory;
+
+    @Bean("redisTemplateJackson")
+    @Qualifier("redisTemplateJackson")
+    public RedisTemplate<String,String> redisTemplateJackson(){
+        StringRedisTemplate template = new StringRedisTemplate(lettuceConnectionFactory);
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        template.afterPropertiesSet();
+
+        // Long类型不可以会出现异常信息;
+        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
+        template.setKeySerializer(redisSerializer);
+        template.setHashKeySerializer(redisSerializer);
+
+        return template;
+    }
+}
