6e99920bac34db095e015dc4622ff26072dfbacd
yuhan.tang <yuhan.tang@magicwindow.cn>
2018-07-19 12:16:59 +0800

    edit swagger2

--- entertainment-asset/src/main/java/com/entertainment/asset/Swagger2.java
+++ entertainment-asset/src/main/java/com/entertainment/asset/Swagger2.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
@@ -33,13 +35,14 @@ public class Swagger2 {
 
     @Bean
     public Docket createRestApi(){
+        ParameterBuilder tokenPar = new ParameterBuilder();
         List<Parameter> pars = new ArrayList<>();
-//        tokenPar.name("Authorization").description("令牌").modelRef(new ModelRef("string")).parameterType("header")
+        tokenPar.name("Authorization").description("令牌").modelRef(new ModelRef("string")).parameterType("header")
 //        .defaultValue(token)
-//        .required(false);
-//        pars.add(tokenPar.build());
-//        tokenPar.name("locale").description("语言").modelRef(new ModelRef("string")).parameterType("query").defaultValue("ch").required(false);
-//        pars.add(tokenPar.build());
+        .required(false);
+        pars.add(tokenPar.build());
+        tokenPar.name("locale").description("语言").modelRef(new ModelRef("string")).parameterType("query").defaultValue("ch").required(false);
+        pars.add(tokenPar.build());
 //        //隐藏生产swagger地址
         Predicate<String> pathSelectors = PathSelectors.any();
         if(profiles.contains("prod")){
