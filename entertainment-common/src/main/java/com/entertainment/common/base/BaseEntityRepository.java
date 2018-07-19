6fd66c530e6210da44002b24d4f9431e7e6a4986
HuangJiaHao <1183665198@q.com>
2018-07-19 09:22:47 +0800

    导包

--- entertainment-common/src/main/java/com/entertainment/common/base/BaseEntityRepository.java
+++ entertainment-common/src/main/java/com/entertainment/common/base/BaseEntityRepository.java
@@ -1,10 +1,11 @@
 package com.entertainment.common.base;
 
+import com.entertainment.common.entity.base.BaseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
 @NoRepositoryBean
 public interface BaseEntityRepository <T extends BaseEntity> extends JpaRepository<T, Long> {
 
-    T findOneById(Long id);
+
 }
