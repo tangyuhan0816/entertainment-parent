31b09a5b30aba475b64612498750c5cb4a8f812d
HuangJiaHao <1183665198@q.com>
2018-07-25 10:01:19 +0800

    用户注册

--- entertainment-asset/src/main/java/com/entertainment/asset/controller/sys/RegController.java
+++ entertainment-asset/src/main/java/com/entertainment/asset/controller/sys/RegController.java
@@ -1,23 +1,11 @@
 package com.entertainment.asset.controller.sys;
 
-import com.entertainment.asset.entity.sys.SysUser;
 import com.entertainment.asset.service.sys.SysUserService;
-import com.entertainment.asset.config.EmailUtil;
 import com.entertainment.common.utils.ResponseContent;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.*;
 
 
-/**
- * @Author: Yuhan.Tang
- * @ClassName: SysUserController
- * @package: com.entertainment.asset.controller.sys
- * @Date: Created in 2018/7/20 下午6:03
- * @email yuhan.tang@magicwindow.cn
- * @Description:
- */
 @RestController
 public class RegController {
 
