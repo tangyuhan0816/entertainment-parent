ea23ebfbb0a698f488d34d0236ad0ec80b8a301f
HuangJiaHao <1183665198@qq.com>
2018-07-25 11:04:01 +0800

    修改注解

--- entertainment-asset/src/main/java/com/entertainment/asset/controller/sys/SysUserController.java
+++ entertainment-asset/src/main/java/com/entertainment/asset/controller/sys/SysUserController.java
@@ -1,24 +1,18 @@
 package com.entertainment.asset.controller.sys;
 
-import com.entertainment.asset.entity.sys.SysUser;
 import com.entertainment.asset.service.sys.SysUserService;
 import com.entertainment.common.page.PageableRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.*;
 
 
 /**
- *  @Author: Yuhan.Tang
- *  @ClassName: SysUserController
- *  @package: com.entertainment.asset.controller.sys
- *  @Date: Created in 2018/7/20 下午6:03
- *  @email yuhan.tang@magicwindow.cn
- *  @Description:
+ * @Author: Yuhan.Tang
+ * @ClassName: SysUserController
+ * @package: com.entertainment.asset.controller.sys
+ * @Date: Created in 2018/7/20 下午6:03
+ * @email yuhan.tang@magicwindow.cn
+ * @Description:
  */
 @RestController
 @RequestMapping("/sysUser")
@@ -27,10 +21,8 @@ public class SysUserController {
     @Autowired
     private SysUserService sysUserService;
 
-    @RequestMapping(path = "/findByPage" , method = {RequestMethod.POST})
-    public Object login(HttpServletRequest request,
-                        @RequestBody final PageableRequest pageableRequest){
-
+    @PostMapping("/findByPage")
+    public Object login(@RequestBody final PageableRequest pageableRequest) {
         return sysUserService.findByPage(pageableRequest);
     }
 }
