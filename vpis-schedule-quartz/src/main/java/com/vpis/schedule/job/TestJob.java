a9a22bcc3f6c8681249568790f786b586448e003
yuhan.tang <yuhan.tang@magicwindow.cn>
2018-11-14 19:02:07 +0800

    优化项目

--- vpis-schedule-quartz/src/main/java/com/vpis/schedule/job/TestJob.java
+++ vpis-schedule-quartz/src/main/java/com/vpis/schedule/job/TestJob.java
@@ -1,22 +1,15 @@
 package com.vpis.schedule.job;
 
-import com.vpis.schedule.service.order.OrderService;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
 public class TestJob extends QuartzJobBean{
-
-    @Autowired
-    private OrderService orderService;
 
     @Override
     protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         System.out.println("开始-----------");
         try {
-            System.out.println(orderService.findByOrderStatus());
-            System.out.println(orderService.findById());
             Thread.sleep(100);
         } catch (InterruptedException e) {
             e.printStackTrace();
