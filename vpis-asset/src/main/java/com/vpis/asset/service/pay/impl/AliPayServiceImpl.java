c58a906ee5c8965e67afbce576436a8dccaab972
yuhan.tang <yuhan.tang@magicwindow.cn>
2018-11-21 11:54:21 +0800

    升级支付宝版本

--- vpis-asset/src/main/java/com/vpis/asset/service/pay/impl/AliPayServiceImpl.java
+++ vpis-asset/src/main/java/com/vpis/asset/service/pay/impl/AliPayServiceImpl.java
@@ -45,29 +45,22 @@ public class AliPayServiceImpl implements IPayService{
                 AlipayConfig.ALIPAY_SIGN_TYPE);
         // 创建APP支付订单信息
         AlipayTradeAppPayRequest appPayReq = new AlipayTradeAppPayRequest();
-        appPayReq.setNotifyUrl(notifyUrl);
 
-        // 请求参数
         AlipayTradeAppPayModel model = new AlipayTradeAppPayModel();
-        // 以下业务参数请按实际业务情况填写
-        // ----------------------------------------------------------------
-        // 该笔订单允许的最晚付款时间，逾期将关闭交易。取值范围：1m～15d
-        // m-分钟，h-小时，d-天，1c-当天（1c-当天的情况下，无论交易何时创建，都在0点关闭）。 该参数数值不接受小数点， 如 1.5h，可转换为 90m。 非必填 类型String 最大长度6
+
         model.setTimeoutExpress(TIMEOUT);
 
-        // 订单总金额，单位为元，精确到小数点后两位，取值范围[0.01,100000000]   [建议填写] 非必填 类型String 最大长度9
         model.setTotalAmount(payRequest.getOrderAmount().toString());
 
-        // 对一笔交易的具体描述信息。如果是多种商品，请将商品描述字符串累加传给body。  非必填 类型String 最大长度128
         model.setBody(payRequest.getOrderName());
 
-        // 商品的标题/交易标题/订单标题/订单关键字等。  非必填 类型String 最大长度256
         model.setSubject(payRequest.getSubject());
 
-        // 商户网站唯一订单号 型String 最大长度64
         model.setOutTradeNo(payRequest.getOrderId());
 
         appPayReq.setBizModel(model);
+
+        appPayReq.setNotifyUrl(notifyUrl);
         // 发起请求
         AlipayTradeAppPayResponse response = null;
         try {
